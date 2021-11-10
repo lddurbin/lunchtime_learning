9f22ba4628e163bf6ea8134b038306f246087711
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2021-11-10 00:29:02 +0000

    Re-factored code, facet on gears.

--- basic_plots.R
+++ basic_plots.R
@@ -1,9 +1,11 @@
 library(ggplot2)
 
-ggplot(mtcars) +
-  geom_smooth(aes(x = mpg, y = hp)) +
-  geom_point(aes(x = mpg, y = hp), alpha = 0.5) +
+# Create a scatterplot
+ggplot(mtcars, aes(x = mpg, y = hp)) +
+  geom_smooth() +
+  geom_point(alpha = 0.5) +
   labs(
     title = "Cars with more miles per gallon have less horsepower",
     caption = "Data: 1974 Motor Trend US magazine"
-    )
+    ) +
+  facet_wrap(~gear)
