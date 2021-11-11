84474c0b7d01e805ce43c29062d98acf4da59fde
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2021-11-11 00:22:37 +0000

    Finished scatterplot, created bar chart

--- basic_plots.R
+++ basic_plots.R
@@ -1,11 +1,20 @@
 library(ggplot2)
+library(ggthemes)
 
 # Create a scatterplot
-ggplot(mtcars, aes(x = mpg, y = hp)) +
+scatterplot <- ggplot(mtcars, aes(x = mpg, y = hp)) +
   geom_smooth() +
   geom_point(alpha = 0.5) +
   labs(
     title = "Cars with more miles per gallon have less horsepower",
     caption = "Data: 1974 Motor Trend US magazine"
     ) +
-  facet_wrap(~gear)
+  facet_wrap(~gear) +
+  theme_wsj()
+
+ggsave("scatterplot.png", scatterplot)
+
+barplot <- ggplot(diamonds, aes(x = clarity)) +
+  geom_bar()
+
+ggsave("barplot.png", barplot)
