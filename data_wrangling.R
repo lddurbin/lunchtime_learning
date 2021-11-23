00beeb8be9507d878002f1d60684d00c676c7f96
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2021-11-23 00:32:51 +0000

    Introduced select and mutate

--- data_wrangling.R
+++ data_wrangling.R
@@ -12,3 +12,9 @@ female_employees <- filter(raw_data, gender == "F")
 # Sort by middle initial (ascending order) and last name (descending order)
 filtered_and_arranged <- filter(raw_data, year_of_joining < 2010 & gender != "M" & !middle_initial %in% c("A", "B", "C", "D")) %>%
   arrange(middle_initial, desc(last_name))
+
+# Select emp_id and all columns that end with "name"
+# Create a new column called full_name, which concatenated first_name, last_name, and emp_id. Drop the colummns used to create it
+filtered_and_arranged %>% 
+  select(emp_id, ends_with("name")) %>% 
+  mutate(full_name = paste0(first_name, " ", last_name, " (", emp_id, ")"), .keep = "unused")
