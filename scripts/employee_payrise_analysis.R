2209232168daa3e0da3860bb45146f0e04274623
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2021-11-30 00:31:17 +0000

    Complete pay rise analysis

--- scripts/employee_payrise_analysis.R
+++ scripts/employee_payrise_analysis.R
@@ -13,3 +13,24 @@ pay_rise_bands <- pay_rise %>%
     salary < 75000 ~ "$50,000 to $74,999",
     salary < 100000 ~ "$75,000 to $99,999"
   ))
+
+pay_rise_bands %>% 
+  count(salary_band)
+
+pay_rise_bands %>% 
+  mutate(pay_rise_1_pct = round(salary*1.01, 0)) %>% 
+  group_by(salary_band) %>% 
+  summarise(investment_total = sum(pay_rise_1_pct), avg_old_salary = round(mean(salary), 0), avg_new_salary = round(mean(pay_rise_1_pct), 0))
+
+pay_rise_bands %>% 
+  mutate(low_pay_shortfall = case_when(
+    salary_band == "Less than $50,000" ~ (50000-salary)/salary,
+    TRUE ~ 1
+  )) %>% 
+  mutate(new_pay = case_when(
+    salary_band == "Less than $50,000" ~ salary*(1+low_pay_shortfall),
+    salary_band == "$50,000 to $74,999" ~ round(salary*1.025, 0),
+    salary_band == "$75,000 to $99,999" ~ round(salary*1.01, 0)
+  )) %>% 
+  group_by(salary_band) %>% 
+  summarise(investment_total = sum(new_pay), avg_increase = round(mean(new_pay-salary), 0))
